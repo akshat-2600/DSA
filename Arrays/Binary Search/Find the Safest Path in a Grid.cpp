f1a2b6aa2052ca7a22675f4a2b75d17a00b17eb6
Akshat Saxena <akshatsaxena890@gmail.com>
2026-07-02 14:25:33 +0530

    LeetCode POTD 1st July 2026

--- Arrays/Binary Search/Find the Safest Path in a Grid.cpp
+++ Arrays/Binary Search/Find the Safest Path in a Grid.cpp
@@ -1,5 +1,5 @@
 /*
-Company Tags                : GOOGLE
+    Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/find-the-safest-path-in-a-grid/description/
 */
 
@@ -113,3 +113,112 @@ public:
         return result;
     }
 };
+
+
+/************************************************************ JAVA ************************************************************************/
+
+//T.C : O(log(n) * n^2)
+//S.C : O(n^2)
+
+class Solution {
+    int n;
+    int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+
+    boolean check(int[][] distNearestThief, int sf) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        q.add(new int[]{0, 0});
+        visited[0][0] = true;
+
+        if (distNearestThief[0][0] < sf) {
+            return false;
+        }
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int curr_i = curr[0];
+            int curr_j = curr[1];
+
+            if (curr_i == n-1 && curr_j == n-1) {
+                return true;
+            }
+
+            for (int[] dir: directions) {
+                int new_i = curr_i + dir[0];
+                int new_j = curr_j + dir[1];
+
+                if (new_i >= 0 && new_i < n && new_j >= 0 && new_j < n && !visited[new_i][new_j]) {
+                    if (distNearestThief[new_i][new_j] < sf) {
+                        continue;
+                    }
+                    q.add(new int[]{new_i, new_j});
+                    visited[new_i][new_j] = true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        n = grid.size();
+
+        //Step-1 Precalculation of distNearestThief - for each cell
+        int[][] distNearestThief = new int[n][n];
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        //Push all cells in queue where thieves are present
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    q.add(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                int[] curr = q.poll();
+                int curr_i = curr[0];
+                int curr_j = curr[1];
+
+                distNearestThief[curr_i][curr_j] = level;
+
+                for (int[] dir: directions) {
+                    int new_i = curr_i + dir[0];
+                    int new_j = curr_j + dir[1];
+
+                    if (new_i < 0 || new_i >= n || new_j < 0 || new_j >= n || visited[new_i][new_j]) {
+                        continue;
+                    }
+
+                    q.add(new int[]{new_i, new_j});
+                    visited[new_i][new_j] = true;
+                }
+            }
+            level++;
+        }
+
+        //Step-2 Apply binary search on SF
+        int l = 0;
+        int r = 400;
+        int result = 0;
+
+        while (l <= r) {
+            int mid_sf = l + (r - l)/2;
+
+            if (check(distNearestThief, mid_sf)) {
+                result = mid_sf;
+                l = mid_sf+1;
+            } else {
+                r = mid_sf-1;
+            }
+        }
+        return result;
+    }
+}
