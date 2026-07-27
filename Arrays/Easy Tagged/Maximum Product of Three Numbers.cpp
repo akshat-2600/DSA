8edb3278e4e84a60efbbef3dbd7f85c389b22ae8
Akshat Saxena <akshatsaxena890@gmail.com>
2026-07-27 10:00:46 +0530

    LeetCode 628. Optimized approach

--- Arrays/Easy Tagged/Maximum Product of Three Numbers.cpp
+++ Arrays/Easy Tagged/Maximum Product of Three Numbers.cpp
@@ -53,6 +53,46 @@ public:
     }
 };
 
+// Approach : Without Sorting
+// T.C : O(n)
+// S.C : O(1)
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int max3 = INT_MIN;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        for (int &num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            }
+            else if (num > max3) {
+                max3 = num;
+            }
+
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            }
+            else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return max(max1 * max2 * max3, max1 * min1 * min2);
+    }
+};
+
 
 /**************************************************************** JAVA ****************************************************************/
 
@@ -103,6 +143,45 @@ class Solution {
         } else {
             return maxRight;
         }
+    }
+}
+
+// Approach : Without Sorting
+// T.C : O(n)
+// S.C : O(1)
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            // Update the top three maximum values
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+
+            // Update the top two minimum values
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        // Return the maximum of the two possible products
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
     }
 }
 
