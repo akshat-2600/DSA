bb097821630a35a19b320f2d1e0eb872170b7d6a
Akshat Saxena <akshatsaxena890@gmail.com>
2026-08-14 13:10:04 +0530

    LeetCode POTD 14th August 2026

--- Sliding Window/Maximum Length Substring With Two Occurrences.cpp
+++ Sliding Window/Maximum Length Substring With Two Occurrences.cpp
@@ -1,6 +1,6 @@
 /*
     Company Tags    :   
-    LeetCode Link   :   https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/description/
+    LeetCode Link   :   https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/description/
 */
 
 
@@ -8,52 +8,58 @@
 
 // Approach : Sliding Window
 // T.C      : O(n)
-// S.C      : O(n)
+// S.C      : O(1)
 
 class Solution {
-    public int maxSubarrayLength(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer, Integer> mp = new HashMap<>();
+public:
+    int maximumLengthSubstring(string s) {
+        int n = s.length();
+        vector<int> vec(26, 0);
 
         int i = 0;
         int j = 0;
         int result = 0;
 
         while (j < n) {
-            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            char ch1 = s[j];
+            vec[ch1 - 'a']++;
 
-            while (i < j && mp.get(nums[j]) > k) {
-                mp.put(nums[i], mp.get(nums[i]) - 1);
+            while (i < j && vec[ch1 - 'a'] > 2) {
+                char ch2 = s[i];
+                vec[ch2 - 'a']--;
                 i++;
             }
 
-            result = Math.max(result, j - i + 1);
+            result = max(result, j - i + 1);
             j++;
         }
         return result;
     }
-}
+};
+
 
 /**************************************************************** JAVA ****************************************************************/
 
 // Approach : Sliding Window
 // T.C      : O(n)
-// S.C      : O(n)
+// S.C      : O(1)
 
 class Solution {
-    public int maxSubarrayLength(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer, Integer> mp = new HashMap<>();
+    public int maximumLengthSubstring(String s) {
+        int n = s.length();
+        int[] mp = new int[26];
 
         int i = 0;
         int j = 0;
         int result = 0;
 
         while (j < n) {
-            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            char ch1 = s.charAt(j);
+            mp[ch1 - 'a']++;
 
-            while (i < j && mp.get(nums[j]) > k) {
-                mp.put(nums[i], mp.get(nums[i]) - 1);
+            while (i < j && mp[ch1 - 'a'] > 2) {
+                char ch2 = s.charAt(i);
+                mp[ch2 - 'a']--;
                 i++;
             }
 
@@ -63,5 +69,3 @@ class Solution {
         return result;
     }
 }
-
-
