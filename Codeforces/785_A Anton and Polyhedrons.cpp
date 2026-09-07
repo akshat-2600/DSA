1a801de5cff61fcb60811b0898b53310d6a6d859
Akshat Saxena <akshatsaxena890@gmail.com>
2026-09-07 23:07:56 +0530

    CodeForces 785_A Anton and Polyhedrons

---
+++ Codeforces/785_A Anton and Polyhedrons.cpp
@@ -0,0 +1,30 @@
+// T.C : O(n)
+// S.C : O(1)
+
+#inlcude <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int total = 0;
+    
+    while (n--) {
+        string s;
+        cin >> s;
+        
+        if (s == "Tetrahedron") {
+            total += 4;
+        } else if (s == "Cube") {
+            total += 6;
+        } else if (s == "Octahedron") {
+            total += 8;
+        } else if (s == "Dodecahedron") {
+            total += 12;
+        } else if (s == "Icosahedron") {
+            total += 20;
+        }
+    }
+    cout << total << endl;
+}
